e2ec619ca5fe416380591237e10dfdf7d5bb8505
Vage <vage.melikbekyan@gmail.com>
2016-06-03 13:26:03 +0300

    Finish user signup

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-    email "MyString"
+    name     "Michael Hartl"
+    email    "michael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
