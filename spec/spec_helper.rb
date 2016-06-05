11bf52c0b1382f769648be424b6f9c677faf8671
Vage <vage.melikbekyan@gmail.com>
2016-06-05 13:23:41 +0300

    Add user microposts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,10 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/rails'
 require 'rspec/autorun'
+require "will_paginate"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
